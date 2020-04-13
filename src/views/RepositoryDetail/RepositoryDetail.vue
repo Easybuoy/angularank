752ef0263e0d911bcad74cf1c58e17620c0a6f1f
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-04-13 17:34:22 +0100

    repository detail

---
+++ src/views/RepositoryDetail/RepositoryDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>HHH</h1>
+</template>
+
+<script>
+export default {
+  name: 'Repository_Detail',
+  created() {
+    const { login, repo_name } = this.$route.params;
+    console.log(login, repo_name);
+  }
+};
+</script>
+
+<style scoped lang="less">
+</style>
