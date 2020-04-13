032fed41dbf64c9f175317a32ca2d07c64c78b6a
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-04-13 09:47:10 +0100

    add spinner component

--- src/components/UserDetail/UserDetail.vue
+++ src/components/UserDetail/UserDetail.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
-    {{ this.userDetail }}
+    <div class="text-center d-flex justify-center align-center spinner" v-if="loading">
+      <v-progress-circular
+        :size="50"
+        :width="5"
+        color="red accent-4"
+        indeterminate
+      ></v-progress-circular>
+    </div>
+
+    <div v-else-if="error !== null" class="err mt-5">
+      <v-alert type="error" dismissible="true">{{ error }}</v-alert>
+
+      <a @click="this.reloadPage">
+        <v-btn class="ma-2" tile color="success" dark>Reload Page</v-btn>
+      </a>
+    </div>
+
+    <div v-else>
+      {{ userDetail }}
+      <v-img
+        :src="userDetail.avatar_url"
+        :lazy-src="userDetail.avatar_url"
+        aspect-ratio="1"
+        class="grey lighten-2"
+      ></v-img>
+    </div>
   </div>
 </template>
 
@@ -15,7 +40,7 @@ export default {
     console.log(this.userDetail, 'userd');
   },
   computed: {
-    ...mapGetters(['userDetail'])
+    ...mapGetters(['userDetail', 'loading', 'error'])
   },
   methods: {
     ...mapActions(['getUserDetail'])
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -3,25 +3,34 @@ import axios from 'axios';
 import { formatURL, extractURL, fetchURL } from '../../utils';
 
 const state = {
-  userDetail: {}
+  userDetail: {},
+  loading: false,
+  error: null
 };
 
 const getters = {
-  userDetail: (currentState) => currentState.userDetail,
+  userDetail: currentState => currentState.userDetail,
+  loading: currentState => currentState.loading,
+  error: currentState => currentState.error,
 };
 
 const actions = {
   async getUserDetail({ commit }, login) {
-    // console.log(login);
+    commit('setLoading');
     axios
       .get(`https://api.github.com/users/${login}`)
-      .then((res) => commit('setUserDetail', res.data))
-      .catch((err) => console.log(err));
+      .then(res => {
+        // commit('setLoading');
+        // commit('setUserDetail', res.data);
+      })
+      .catch(err => console.log(err));
   }
 };
 
 const mutations = {
-  setUserDetail: (state, userDetail) => (state.userDetail = userDetail)
+  setUserDetail: (state, userDetail) => (state.userDetail = userDetail),
+  setLoading: state => (state.loading = !state.loading),
+  setError: (state, error) => (state.error = error)
 };
 
 export default {
